8a8da7ade8ae35f162001e7554c4eb790f351f3a
Arun Gimblett <gimblett44@gmail.com>
2015-09-28 13:54:43 +0800

    small changes to try and run the 100,000,000 for NSUB

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 x=0;
 for ii in $(seq 7)
 do
-	rm -f  output/vanilla$ii.txt
-	for i in $(seq 100)
+	rm -f  output/vanilla_$ii.txt
+	for i in $(seq 5)
 	do
 		echo $i
 		x=$(echo "scale=20; 10^$ii" | bc);
 		echo $x;
 		echo $(date +"%T")
 
-		./p1 $x >> output/vanilla$ii.txt;
+		./p1 $x >> output/vanilla_$ii.txt;
 		echo $(date +"%T");
 	done
 done
@@ -22,7 +22,7 @@ for ii in $(seq 9)
 do
 	x=0;
 	size=0;
-	filelines=`cat output/vanilla$ii.txt`
+	filelines=`cat output/vanilla_$ii.txt`
 	for line in $filelines; do
 
 		size=$(($size+1));
