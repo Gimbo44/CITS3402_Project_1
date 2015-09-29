0e204171cf2514192bf2359159daf4c18ff09433
Arun Gimblett <gimblett44@gmail.com>
2015-09-29 21:04:37 +0800

    small changes to run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 x=0;
 for ii in $(seq 7)
 do
-	rm -f  output/vanilla_$ii.txt
-	for i in $(seq 5)
+	rm -f  output/4.0_$ii.txt
+	for i in $(seq 20)
 	do
 		echo $i
 		x=$(echo "scale=20; 10^$ii" | bc);
 		echo $x;
 		echo $(date +"%T")
 
-		./p1 $x >> output/vanilla_$ii.txt;
+		./p1 $x >> output/4.0_$ii.txt;
 		echo $(date +"%T");
 	done
 done
@@ -18,20 +18,21 @@ done
 
 
 
-for ii in $(seq 9)
+for ii in $(seq 7)
 do
 	x=0;
 	size=0;
-	filelines=`cat output/vanilla_$ii.txt`
+	filelines=`cat output/4.0_$ii.txt`
 	for line in $filelines; do
 
 		size=$(($size+1));
 		x=$(echo "scale=20; $x+$line" | bc)
-		echo "Vanilla $ii:"
-		echo "scale=10; $x/$size" | bc
+		
 
 
 	done
+	echo "Vanilla $ii:"
+	echo "scale=10; $x/$size" | bc
 done
 
 
